9a5096dfcdfe1e536844b7602a93827db19346e2
Aymeric Gallissot <hello@aymericgallissot.fr>
2013-03-23 00:40:20 +0100

    Initial commit

---
+++ evasion/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  evasion
+//
+//  Created by Aymeric on 23/03/13.
+//  Copyright (c) 2013 Fuzzze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
